7f726c6959f69fd9063d708b27a5cd7620a094f6
Kaillr <125704518+Kaillr@users.noreply.github.com>
2025-05-12 11:46:24 +0200

    reinitiate template

---
+++ auth/eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    globalIgnores(['dist/']),
+]);
---
+++ client/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+    clearScreen: false,
+    server: {
+        strictPort: true,
+        port: 3000
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components')
+        }
+    },
+    envPrefix: ['VITE_', 'TAURI_'],
+    build: {
+        target: ['es2021', 'chrome100', 'safari13'],
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        sourcemap: !!process.env.TAURI_DEBUG
+    },
+    plugins: [react(), tailwindcss()]
+});
